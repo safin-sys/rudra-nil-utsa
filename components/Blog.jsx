449a07c3651f67ea99d10ada66203b216eb9b60d
safin-sys <mdsafinofficial@gmail.com>
2021-03-24 21:29:31 +0600

    blogs api connected

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,36 +1,13 @@
 import style from '../styles/blog/Blog.module.scss'
 import { AiOutlineArrowRight } from "react-icons/ai"
 
-const blogs = [
-    {title: "Marshall Mathers", date: "1635728586"},
-	{title: "Abel M. Tesfaye", date: "1601689917"},
-	{title: "Taylor Swift", date: "1590316540"},
-	{title: "Brennan Witt", date: "1644188407"},
-	{title: "Denton Mendoza", date: "1644157562"},
-	{title: "Conan Larsen", date: "1585762993"},
-	{title: "Benjamin Beck", date: "1641193978"},
-	{title: "Carl Guerra", date: "1600514872"},
-	{title: "Gregory Barron", date: "1620673955"},
-	{title: "Ishmael Frank", date: "1641139465"},
-	{title: "Kenneth Bowers", date: "1630962435"},
-	{title: "Merritt Hayden", date: "1619591434"},
-	{title: "Orlando Barton", date: "1601636069"},
-	{title: "Carlos Stevenson", date: "1629596421"},
-	{title: "Thor Burris", date: "1607112655"},
-	{title: "Scott Olson", date: "1632230572"},
-	{title: "Baxter King", date: "1619392768"},
-	{title: "Lucius Maldonado", date: "1639656709"},
-	{title: "Cedric Tillman", date: "1599048555"},
-	{title: "Sean Herman", date: "1627049251"}
-]
-
 const content = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor consequatur nesciunt obcaecati ut optio ipsam quisquam facere culpa et corporis impedit delectus maiores dolores praesentium fugiat, quaerat hic numquam? Aut."
 
-const Blog = () => {
+const Blog = ({ blogs }) => {
     return (
         <main className={`${style.main} container`}>
             {blogs.map((blog, i) => {
-                 return <Card key={i} title={blog.title} date={blog.date} content={content} />
+                 return <Card key={i} title={blog.title} date={blog.published} link={blog.url} />
             })}
         </main>
     )
@@ -38,18 +15,16 @@ const Blog = () => {
 
 export default Blog
 
-const Card = ({ title, date, content }) => {
+const Card = ({ title, date, link }) => {
     const handleDate = () => {
-        const num = parseInt(date)
-        const d = new Date(num)
+        const d = new Date(date)
         return d
     } 
     handleDate()
     return (
-        <a href="#" className={style.cards}>
+        <a href={link} target="_blank" className={style.cards}>
             <h1>{title}</h1>
             <h6>{`${handleDate().getDate()}/${handleDate().getMonth() + 1}/${handleDate().getFullYear()}`} {`${handleDate().getHours()}:${handleDate().getMinutes()}`}</h6>
-            <p>{content}</p>
             <button>Read More <AiOutlineArrowRight/></button>
         </a>
     )
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,25 @@
 import Head from "next/head"
+import { useEffect } from "react"
 import Blog from "../../components/Blog"
 import Divider from "../../components/Divider"
 import Footer from "../../components/Footer"
 import Hero from "../../components/Hero"
 import Navbar from "../../components/Navbar"
 
-const BlogPage = () => {
+export const getStaticProps = async () => {
+    const link = "https://www.googleapis.com/blogger/v3/blogs/990517938631563196/posts?callback=handleResponse&key=AIzaSyAJESQB3ddltUcDbZif3LUnX-Gzr18tBRg";
+
+    const res = await fetch(link);
+    const data = await res.text();
+    
+    const blogs = JSON.parse(data.replace(/^[^\(]*\(/, '').replace(/\);$/, ''));
+
+    return {
+        props: { blogs: blogs.items }
+    }
+}
+
+const BlogPage = ({ blogs }) => {
     return (
         <>
             <Head>
@@ -14,7 +28,7 @@ const BlogPage = () => {
             <Navbar />
             <Hero title="Welcome To My Blog" />
             <Divider direction="right" />
-            <Blog />
+            <Blog blogs={blogs} />
             <Footer />
         </>
     )
