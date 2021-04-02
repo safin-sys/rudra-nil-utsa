597d8e621c6a6aee523f42a465591385c75873e2
safin-sys <mdsafinofficial@gmail.com>
2021-04-02 23:25:22 +0600

    softwares page

---
+++ components/Soft.jsx
@@ -0,0 +1,29 @@
+import style from '../styles/soft/Soft.module.scss'
+
+const Soft = ({ softs, langColor }) => {
+    return (
+        <div className="container">
+            {softs.map((soft, i) => {
+                return <Cards key={i} soft={soft} langColor={langColor[soft.lang]} />
+            })}
+        </div>
+    )
+}
+
+const Cards = ({ soft, langColor }) => {
+    return (
+        <a href="#" className={style.soft}>
+            <h1 className={`${style.title} gradientTitle`}>{soft.title}</h1>
+            <p className={style.des}>{soft.des}</p>
+            <div className={style.bottom}>
+                <div className={style.lang}>
+                    <span style={{backgroundColor: langColor}}></span>
+                    <p>{soft.lang}</p>
+                </div>
+                <p>{soft.plat}</p>
+            </div>
+        </a>
+    )
+}
+
+export default Soft
--- pages/soft/index.js
+++ pages/soft/index.js
@@ -1,9 +1,75 @@
 import Head from 'next/head'
 import Footer from '../../components/Footer'
+import Divider from '../../components/Divider'
 import Hero from '../../components/Hero'
 import Navbar from '../../components/Navbar'
+import Soft from '../../components/Soft'
 
-const SoftwarePage = () => {
+export const getStaticProps = async () => {
+    const softs = [
+        {
+            title: 'ZREC',
+            des: 'ZREC is a zlib (p)recompressor',
+            lang: 'Pascal',
+            plat: 'Windows (64bit)'
+        },
+        {
+            title: 'PassMan',
+            des: 'PassMan is a password manager for everyday use',
+            lang: 'Pascal',
+            plat: 'Android / Windows (32bit)'
+        },
+        {
+            title: 'Image2PDF',
+            des: 'Converts a bunch of images to a single pdf file.',
+            lang: 'Pascal',
+            plat: 'Windows'
+        },
+        {
+            title: 'The Chromium Protector',
+            des: 'A Password Protector for your browsers',
+            lang: 'Pascal',
+            plat: 'Windows (64bit)'
+        },
+        {
+            title: 'Image2PDF Android',
+            des: 'Converts a bunch of images to a single pdf file.',
+            lang: 'Pascal',
+            plat: 'Android'
+        },
+        {
+            title: 'Netlock',
+            des: 'A simple program to block websites for your device',
+            lang: 'Pascal',
+            plat: 'Windows (64bit) / Windows (32bit)'
+        },
+        {
+            title: 'Desktop-Locker',
+            des: 'A regular desktop locker that requires password to unlock. The application captures a photo using webcam if user fails to write the correct password.',
+            lang: 'Pascal',
+            plat: 'Windows (64bit) / Windows (32bit)'
+        }
+    ]
+    const langColor = {};
+    const url = "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json";
+    const res = await fetch(url)
+    const data = await res.json();
+
+    softs.forEach(soft => {
+        if (data[soft.lang]) {
+            langColor[soft.lang] = data[soft.lang]['color'];
+        }
+    })
+
+    return {
+        props: {
+            softs,
+            langColor
+        }
+    }
+}
+
+const SoftwarePage = ({ softs, langColor }) => {
     return (
         <>
             <Head>
@@ -11,6 +77,8 @@ const SoftwarePage = () => {
             </Head>
             <Navbar />
             <Hero title="Softwares I Made" />
+            <Divider />
+            <Soft softs={softs} langColor={langColor} />
             <Footer />
         </>
     )
