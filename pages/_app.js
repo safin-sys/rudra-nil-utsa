223242f255eab537c928541f10c358b3d65c64d3
Safin Ahmed <mdsafinofficial@gmail.com>
2021-04-16 22:50:07 +0600

    accessabilty and SEO 💯

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/global.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
