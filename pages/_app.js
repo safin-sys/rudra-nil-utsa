2fe7020f91c3b13ee0b3682f2aab143dcf8fa69e
Safin Ahmed <mdsafinofficial@gmail.com>
2021-12-01 23:41:36 +0600

    google site verification

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="uR73wVtmcchCUfsHMV8S2bJc2qHxSQAiC5p1wXTd1Fw" />
       </Head>
       <Component {...pageProps} />
     </>
